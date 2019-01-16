b273e65d5ecda8633fee0cc61c07e051f7b4b586
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-01-16 16:32:24 +0300

    Enable Static Files

---
+++ App_Server/App.js
@@ -0,0 +1,16 @@
+var express = require("express");
+var app  = express();
+var path = require("path");
+//#region COMMON TASK
+                // 1-) Enable Static Files
+                // 2-) Definition jade,pug file 
+                // 3-) Connect to database 
+                // 4-) Model Creator
+                // 5-) Configure Route Manager
+//#endregion
+//#region 1-) Enable Static Files
+app.use("/App_Server/public/assets",express.static(path.join(__dirname,"public/assets")));
+//#endregion
+app.listen(8000,()=>{
+    console.log("Server is starting....");
+});
