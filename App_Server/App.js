08fad68e4f2d3a48d97e8201b583a100218c5dc1
ertnakpinaroglu <ertanakpinaroglu@gmail.com>
2019-01-16 16:53:05 +0300

    Configure Route Manager using into app.js

--- App_Server/App.js
+++ App_Server/App.js
@@ -1,21 +1,21 @@
 var express = require("express");
-var app  = express();
+var app = express();
 var path = require("path");
 var tryModel = require("../Models/TryModel");
 //#region COMMON TASK
-                // 1-) Enable Static Files
-                // 2-) Definition jade,pug file 
-                // 3-) Connect to database 
-                // 4-) Model Creator
-                // 5-) Configure Route Manager
+// 1-) Enable Static Files
+// 2-) Definition jade,pug file 
+// 3-) Connect to database 
+// 4-) Model Creator
+// 5-) Configure Route Manager
 //#endregion
 //#region 1-) Enable Static Files
-app.use("/App_Server/public/assets",express.static(path.join(__dirname,"public/assets")));
+app.use("/App_Server/public/assets", express.static(path.join(__dirname, "public/assets")));
 //#endregion
 
 //#region  2-) Definition jade,pug file
-app.set("view engine","pug");
-app.set("Views",path.join(__dirname,"../Views"));
+app.set("view engine", "pug");
+app.set("Views", path.join(__dirname, "../Views"));
 //#endregion
 
 //#region 3-) Connect to database
@@ -25,19 +25,21 @@ var dbAccess = require("../DbAccess/DatabaseContext");
 
 //#region  4-) Model Creator
 var newTrymodel = new tryModel({
-    Title:"oylesine",
-    Name:"ertan"
+    Title: "oylesine",
+    Name: "ertan"
 });
-newTrymodel.save(function (err,data) {  
-   if (err) {
-       console.log("Ekleme yapılırken hata oluştu");
-   } 
-   console.log("Ekleme işlemi bşarılı");
+newTrymodel.save(function (err, data) {
+    if (err) {
+        console.log("Ekleme yapılırken hata oluştu");
+    }
+    console.log("Ekleme işlemi bşarılı");
 });
 //#endregion
 
+//#region 5-) Configure Route Manager 
+require("../Route/RouteManager")(app);
+//#endregion
 
-
-app.listen(8000,()=>{
+app.listen(8000, () => {
     console.log("Server is starting....");
 });
